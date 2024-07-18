9b3601a9ae1eed78c569128b5af26f4f6c7cc4a8
erikth97 <erik.tamayohe@uanl.edu.mx>
2024-07-18 16:56:23 -0600

    Fix: Change background logo & Location Google API

--- src/components/Ubicacion.tsx
+++ src/components/Ubicacion.tsx
@@ -8,12 +8,13 @@ const Ubicacion: React.FC = () => {
         <Grid item xs={12} md={6} sx={{ display: 'flex', justifyContent: 'center' }}>
           <iframe
             title="Ubicación en Google Maps"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3733.489986288736!2d-100.23965698557012!3d20.67642190587871!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842c8efab3b62217%3A0x91a5082024a6f3a!2sC.%20Guadalupe%20Victoria%20100%2C%20Tamaulipas%2C%2067190%20Guadalupe%2C%20N.L.%2C%20Mexico!5e0!3m2!1sen!2sus!4v1674979229871!5m2!1sen!2sus"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3596.286189859992!2d-100.14685702537273!3d25.66180611272712!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8662c241b0b9ffb1%3A0x4c18f9f5c1910a2d!2sC.S.U%20Tamaulipas!5e0!3m2!1ses-419!2smx!4v1721338662287!5m2!1ses-419!2smx"
             width="600"
             height="450"
             style={{ border: 0, borderRadius: '8px', boxShadow: '0 4px 12px rgba(0, 0, 0, 0.2)' }}
             allowFullScreen
             loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
         </Grid>
         <Grid item xs={12} md={6}>
@@ -22,19 +23,19 @@ const Ubicacion: React.FC = () => {
               Ubicación
             </Typography>
             <Typography variant="body1" sx={{ fontSize: '1.2rem', color: '#FFFFFF', textAlign: 'center', fontWeight: '500', lineHeight: 1.5 }}>
-            CENTRO DE SALUD TAMAULIPAS
-            <br />
-            Avenida Coahuila 101, Tamaulipas.
-            <br />
-            Guadalupe, N.L., MX
-            <br />
-            C.P. 67190
-            <br />
-            Horario: Lu-Vi de 8:00 AM a 4:00 PM
-            <br />
-            Jurisdicción Sanitaria: 4
-            <br />
-            CLUES: NLSSA002296
+              CENTRO DE SALUD TAMAULIPAS
+              <br />
+              Calle Guadalupe Victoria 100, Tamaulipas.
+              <br />
+              Guadalupe, N.L., MX
+              <br />
+              C.P. 67190
+              <br />
+              Horario: Lu-Vi de 8:00 AM a 4:00 PM
+              <br />
+              Jurisdicción Sanitaria: 4
+              <br />
+              CLUES: NLSSA002296
             </Typography>
           </Box>
         </Grid>
